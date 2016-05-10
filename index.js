8e76fda70222ad8d8d646370431d0d0d9191239f
lykmapipo <lallyelias87@gmail.com>
2016-05-10 14:09:24 +0300

    update os details spec

--- index.js
+++ index.js
@@ -9,13 +9,49 @@
 //dependencies
 // var async = require('async');
 // var _ = require('lodash');
+var os = require('os');
 var macaddress = require('macaddress');
 //----------------
 // utilities
 //----------------
+
+/**
+ * @name macs
+ * @description obtain host mac addresses
+ * @param  {Function} done a callback to invoke on success or error
+ */
 exports.macs = function(done) {
     macaddress.all(done);
 };
+
+
+/**
+ * @name os
+ * @description obtain os host details
+ * @param  {Function} done a callback to invoke on success or error
+ */
+exports.os = function(done) {
+    try {
+        var host = {};
+        host.arch = os.arch();
+        host.cpus = os.cpus();
+        host.endianness = os.endianness();
+        host.freemem = os.freemem();
+        // host.homedir = os.homedir();
+        host.hostname = os.hostname();
+        host.networkInterfaces = os.networkInterfaces();
+        host.platform = os.platform();
+        host.release = os.release();
+        host.tmpdir = os.tmpdir();
+        host.totalmem = os.totalmem();
+        host.type = os.type();
+        // host.userInfo = os.userInfo();
+        done(null, host);
+    } catch (e) {
+        done(e);
+    }
+};
+
 
 exports.productKey = function( /*payload, options, done*/ ) {
     // body...
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -7,6 +7,26 @@ var keygen = require(path.join(__dirname, '..'));
 
 describe('keygen', function() {
 
+    it('should be able to obtain host os details', function(done) {
+        keygen.os(function(error, os) {
+
+            //assert
+            expect(os.arch).to.exist;
+            expect(os.cpus).to.exist;
+            expect(os.endianness).to.exist;
+            expect(os.freemem).to.exist;
+            expect(os.hostname).to.exist;
+            expect(os.networkInterfaces).to.exist;
+            expect(os.platform).to.exist;
+            expect(os.release).to.exist;
+            expect(os.tmpdir).to.exist;
+            expect(os.totalmem).to.exist;
+            expect(os.type).to.exist;
+
+            done(error, os);
+        });
+    });
+
     it('should be able to get host mac addresses', function(done) {
         keygen.macs(function(error, macs) {
             expect(error).to.not.exist;
