70b7d57fe4c9e7c2e8efe2c9b70238ddafd0554d
lykmapipo <lallyelias87@gmail.com>
2016-05-10 13:31:32 +0300

    update spec asseertions

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -2,14 +2,16 @@
 
 //dependencies
 var path = require('path');
-// var expect = require('chai').expect;
+var expect = require('chai').expect;
 var keygen = require(path.join(__dirname, '..'));
 
 describe('keygen', function() {
 
     it('should be able to get host mac addresses', function(done) {
         keygen.macs(function(error, macs) {
-            console.log(macs);
+            expect(error).to.not.exist;
+            expect(macs).to.exist;
+            expect(Object.keys(macs)).to.have.length.above(0);
             done(error, macs);
         });
     });
